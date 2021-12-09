0ac3ea206944add202396715c11df06e125c2701
imran khan <imrank2018@namal.edu.pk>
2021-12-09 19:39:52 +0500

    ist commmit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # root "articles#index"
+  root "articles#index"
+  #get "/",to: "articles#index" 
+  get "/articles", to: "articles#index"
+  get "/about", to: "about#index",as: :about
+  # ,as: :about
+end
+
