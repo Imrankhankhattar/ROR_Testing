960586dec6fa55290f08febf81c14758b8bdcb56
imrankhankhattar <imrank2018@namal.edu.pk>
2021-12-13 17:32:47 +0500

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    def index
+        # flash[:notice] = "Successfully created..."
+        # flash[:alert] = "Not created..."
+    end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
   def index
+    flash[:notice] = "Successfully created..."
+    flash[:alert] = "Not created..."
   end
 end
